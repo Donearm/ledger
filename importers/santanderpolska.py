02756e9c942b648188485d61f38aab962bf72667
Gianluca Fiore <gianlucafiore@fountainpencreator.com>
2022-08-16 12:32:51 +0200

    specify headers for SantanderPolska parsing

--- importers/santanderpolska.py
+++ importers/santanderpolska.py
@@ -31,24 +31,38 @@ class SantanderPolskaImporter(importer.ImporterProtocol):
     def __init__(self, account, lastfour):
         self.account = account
         self.lastfour = lastfour
+        self.headers = ['Charge Date', 'Date', 'Description', None, None, 'Amount', 'Balance', 'Index']
 
     def identify(self, f):
         """Regular expression to match the Bank Santander Polska csv export's filename"""
 
-        # remember to add the currency code to the end of the exported file as Bank Millennium saves only the date and time, without any sign of the account name or currency
-        return re.match('statement_[0-9]*\.csv', os.path.basename(f.name))
+        # Santander Polska doesn't make a difference between credit card's statements and saving account's ones in the name
+        # Therefore, the format is identical
+        return re.match('historia_[0-9]*-[0-9]*-[0-9]*_[0-9]*\.csv', os.path.basename(f.name))
 
     def extract(self, f):
         entries = []
 
         with open(f.name) as f:
-            for index, row in enumerate(csv.DictReader(f)):
-                trans_date = parse(row['Transaction date']).date()
-                trans_desc = row['Transaction Type'] + ' ' + row['Description']
-                if row['Debits']:
-                    trans_amt = row['Debits']
-                else:
-                    trans_amt = row['Credits']
+            #for index, row in enumerate(csv.DictReader(f)):
+            for index, row in enumerate(csv.DictReader(f, fieldnames=self.headers)):
+                trans_date = parse(row['Date'], dayfirst=True).date()
+                trans_desc = row['Description']
+                # Santander Polska use periods to separate thousands and commas to separate integer with decimal numbers
+                # As beancount's D function doesn't support this yet (https://github.com/beancount/beancount/issues/204)
+                # it is simpler to just replace the commas with periods in the amount's column
+                trans_amt = row['Amount'].replace(",", ".")
+
+                #trans_date = parse(row[1]).date()
+                #trans_desc = row[2]
+                #trans_amt = row[5]
+
+                #trans_date = parse(row['Transaction date']).date()
+                #trans_desc = row['Transaction Type'] + ' ' + row['Description']
+                #if row['Debits']:
+                #    trans_amt = row['Debits']
+                #else:
+                #    trans_amt = row['Credits']
 
                 meta = data.new_metadata(f.name, index)
 

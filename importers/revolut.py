c9d3805e8afa8748707a285032c08f7bc1d1df20
Gianluca Fiore <gianlucafiore@fountainpencreator.com>
2023-04-03 12:19:31 +0200

    added TRY importer for Revolut statements

--- importers/revolut.py
+++ importers/revolut.py
@@ -159,3 +159,44 @@ class RevolutUSDImporter(importer.ImporterProtocol):
                 entries.append(txn)
 
         return entries
+
+class RevolutTRYImporter(importer.ImporterProtocol):
+    def __init__(self, account, lastfour):
+        self.account = account
+        self.lastfour = lastfour
+
+    def identify(self, f):
+        """Regular expression to match Revolut csv export's filename"""
+
+        return re.match('account-statement_[0-9-_]*_en_TRY_[a-z0-9]*\.csv', os.path.basename(f.name))
+
+    def extract(self, f):
+        entries = []
+
+        with open(f.name) as f:
+            for index, row in enumerate(csv.DictReader(f)):
+                trans_date = parse(row['Started Date']).date()
+                trans_desc = row['Description']
+                trans_amt = row['Amount']
+
+                meta = data.new_metadata(f.name, index)
+
+                txn = data.Transaction(
+                        meta = meta,
+                        date = trans_date,
+                        flag = flags.FLAG_OKAY,
+                        payee = trans_desc,
+                        narration = "",
+                        tags = set(),
+                        links = set(),
+                        postings = [],
+                        )
+
+                txn.postings.append(
+                        data.Posting(self.account, amount.Amount(D(trans_amt), 'TRY'),
+                            None, None, None, None)
+                        )
+
+                entries.append(txn)
+
+        return entries

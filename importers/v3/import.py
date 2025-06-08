0fb1c5918c4bafaf8b74d24e0823ec3b56ab1ee3
Gianluca Fiore <gianlucafiore@fountainpencreator.com>
2025-06-08 18:42:40 +0200

    started writing general import.py file

---
+++ importers/v3/import.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+###############################################################################
+#
+# Copyright (c) 2024, Gianluca Fiore
+#
+###############################################################################
+
+__author__ = "Gianluca Fiore"
+__copyright__ = ""
+__credits__ = ""
+__license__ = ""
+__version__ = ""
+__mantainer__ = ""
+__date__ = ""
+__email__ = ""
+__status__ = ""
+
+# Import bank's importers
+from bankmillennium import MillenniumPLNImporter, MillenniumEURImporter, MillenniumUSDImporter
+from beangulp import Ingest
+import sys
+
+importers = (
+        MillenniumPLNImporter(
+            name="Assets:Bank-Millennium",
+            currency="PLN",
+            patterns={}
+            )
+        )
+
+
+if __name__ == '__main__':
+    ingest = Ingest(importers)
+    ingest()
+
+from beangulp import Importer, Transaction, Posting, Amount, Flag, Metadata
